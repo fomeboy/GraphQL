29d0061bd33b0c6cc2f6fe588e076da530eac19f
Fernando Pires <fomeboy@yahoo.com>
2017-05-22 15:17:19 +0100

    server setup

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require('express')
 const app = express()
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
+
+const schema = buildSchema(
+  `type Query {
+    welcome: String
+  }`
+)
+
+const root = { welcome: () => 'Welcome to Graphql-AI' }
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: false
+})
+)
 
 app.get('/', (req, res) => res.send('hello world'))
 
